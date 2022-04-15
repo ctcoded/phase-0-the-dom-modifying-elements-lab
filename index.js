d167238928dd63f9d6d4a7f08484b7080c137e58
cesartidrick <cesartidrick@gmail.com>
2022-04-15 07:15:57 -0600

    commit tree 0d5600b2f786489462c44388cecbd1572aa10c61

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// Write your code here
+const yourName = 'Cesar'
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+document.body.append(newHeader);
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = `${yourName} is the champion`
+
+console.log(newHeader);
